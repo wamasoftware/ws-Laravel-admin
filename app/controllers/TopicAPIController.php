8f3cc156b396decc705a58e788eda2996b9dd2e4
wamasoftware26 <wamasoftware26@wamasoftware26>
2016-02-12 12:33:07 +0530

    change

--- app/controllers/TopicAPIController.php
+++ app/controllers/TopicAPIController.php
@@ -2,47 +2,45 @@
 
 class TopicAPIController extends \BaseController {
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id) {
+        $statusCode = 200;
+        $response = DB::table('topics')->select('description')->where('id', $id)->get();
+        return Response::xml(array('description' => $response[0]->description), $statusCode);
+    }
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-            try{
-                $statusCode = 200;
-                $response = [
-                  'topics'  => []
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index() {
+        try {
+            $statusCode = 200;
+            $response = [
+                'topics' => []
+            ];
+
+            // $topics = Topic::all();
+            //get all topics
+            $topics = DB::table('topics')->where('status', '1')->get();
+            foreach ($topics as $topic) {
+                $response['topics'][] = [
+                    'id' => $topic->id,
+                    'name' => $topic->name,
+                      
                 ];
-                
-               // $topics = Topic::all();
-                //get all topics
-                $topics = DB::table('topics')->where('status', '1')->get();
-               
-                foreach($topics as $topic){
-
-                    $response['topics'][] = [
-                        'id' => $topic->id,
-                        'name' => $topic->name,
-                        'description' => $topic->description
-                    ];
-                }
-
-            }catch (Exception $e){
-                $statusCode = 400;
             }
-            
-           return Response::xml($response, $statusCode);            
-	}
+        } catch (Exception $e) {
+            $statusCode = 400;
+        }
+
+        return Response::xml($response, $statusCode);
+    }
+
 }
